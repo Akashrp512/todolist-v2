660aac27bba00ba22bac3540b660b13a7a961651
Akash RP <akashrp512@gmail.com>
2022-12-03 18:22:44 +0530

    commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//jshint esversion:6
+ //jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+
+const mongoose =require("mongoose");
 
 const app = express();
 
@@ -11,14 +12,41 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+ mongoose.connect("mongodb://local:27017/todolistDB");
+
+const itemsSchema = {
+  name: String
+};
+
+const Item  = mongoose.model(
+ "Item", itemsSchema
+);
+
+const item1 = new Item({
+  name: "Welcome to your todoList!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1,item2,item3];
+
+Item.insertMany(defaultItems, (err)=>{
+  if(err) {
+    console.log(err);
+  } else{
+    console.log("Successfully saved default item to database");
+  }
+});
 
 app.get("/", function(req, res) {
 
 const day = date.getDate();
 
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
